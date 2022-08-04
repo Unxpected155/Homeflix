82860934ad9698575aea02990936a12ea023ab54
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-04 14:08:11 -0600

    Se agrego la funcion resetVideo

--- HomeFlix/src/main/java/view/ControllerReproductor.java
+++ HomeFlix/src/main/java/view/ControllerReproductor.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
+import javafx.util.Duration;
 
 import java.io.File;
 import java.net.URL;
@@ -29,8 +30,6 @@ public class ControllerReproductor implements Initializable {
     private MediaPlayer mediaPlayer;
 
 
-
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         file = new File("D:\\Videos\\clip.mp4");
@@ -39,11 +38,15 @@ public class ControllerReproductor implements Initializable {
         mediaView.setMediaPlayer(mediaPlayer);
     }
 
-    public void playVideo(){
-    mediaPlayer.play();
+    public void playVideo() {
+        mediaPlayer.play();
     }
 
-    public void pauseVideo(){
-    mediaPlayer.pause();
+    public void pauseVideo() {
+        mediaPlayer.pause();
+    }
+
+    public void resetVideo() {
+        mediaPlayer.seek(Duration.seconds(0.0));
     }
 }
