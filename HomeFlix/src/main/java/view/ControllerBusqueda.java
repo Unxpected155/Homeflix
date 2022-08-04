5309e1ceea19224bb2069ee57ac15c52d61c4a1e
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-04 02:39:56 -0600

    Se creo el ControllerBusqueda con la funcion volverPrincipal

---
+++ HomeFlix/src/main/java/view/ControllerBusqueda.java
@@ -0,0 +1,36 @@
+package view;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerBusqueda {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    private TextField nombreVideo;
+    @FXML
+    private TextField categoria;
+
+    public void botonBuscarVideo(){
+
+    }
+
+    public void volverPrincipal(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
