8108f4c6b9a2cc9b06687daa5ca0e635f1182a13
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-04 14:04:17 -0600

    Se creo la clase ControllerReproductor con la funcion initialize

---
+++ HomeFlix/src/main/java/view/ControllerReproductor.java
@@ -0,0 +1,42 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerReproductor implements Initializable {
+    @FXML
+    private Button pauseButton;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private Button resetButton;
+
+    @FXML
+    private MediaView mediaView;
+
+    private File file;
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        file = new File("D:\\Videos\\clip.mp4");
+        media = new Media(file.toURI().toString());
+        mediaPlayer = new MediaPlayer(media);
+        mediaView.setMediaPlayer(mediaPlayer);
+    }
+
+}
