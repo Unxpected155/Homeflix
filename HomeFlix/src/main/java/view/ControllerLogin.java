0a1a3a5105e271e7021443d7c31fe4ec785fa757
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-04 02:23:02 -0600

    Se creo la funcion botonLogin

--- HomeFlix/src/main/java/view/ControllerLogin.java
+++ HomeFlix/src/main/java/view/ControllerLogin.java
@@ -6,10 +6,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class ControllerLogin {
     @FXML
@@ -22,6 +24,16 @@ public class ControllerLogin {
 
     public void cambiarEscenaRegistro(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Registro.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void botonLogin(ActionEvent event) throws IOException {
+        String usuario = usuarioTF.getText();
+        String contrasenia = contrasenaTF.getText();
+
+        root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
