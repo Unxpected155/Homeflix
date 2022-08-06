f93b1a7204e14b783f6544b21ddecb86a2736bbe
Unxpected155 <gabripb2004@gmail.com>
2022-08-05 19:26:01 -0600

    Se agregaron funciones para calcular el tiempo actual

--- HomeFlix/src/main/java/view/ControllerReproductor.java
+++ HomeFlix/src/main/java/view/ControllerReproductor.java
@@ -1,5 +1,6 @@
 package view;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
@@ -8,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.chart.ValueAxis;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -20,6 +22,7 @@ import javafx.util.Duration;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
 
 public class ControllerReproductor implements Initializable {
     @FXML
@@ -43,6 +46,19 @@ public class ControllerReproductor implements Initializable {
 
     @FXML
     private Button botonFullScreen;
+    @FXML
+    private Label tiempoActualVideo;
+
+    @FXML
+    private Label tiempoTotalVideo;
+    @FXML
+    private ImageView volumenLabel;
+
+    @FXML
+    private Slider volumenSlider;
+    private ImageView imgVolumen;
+
+    private boolean alFinalVideo = false;
 
     private File file;
     private Media media;
@@ -80,7 +96,41 @@ public class ControllerReproductor implements Initializable {
                 mediaPlayer.seek(Duration.seconds(progressBar.getValue()));
             }
         });
+
+        mediaPlayer.volumeProperty().bindBidirectional(volumenSlider.valueProperty());
+
+        tiempoActualVideo();
     }
+
+    public void tiempoActualVideo() {
+        tiempoActualVideo.textProperty().bind(Bindings.createStringBinding(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                return obtenerTiempo(mediaPlayer.getCurrentTime()) + " / ";
+            }
+        }, mediaPlayer.currentTimeProperty()));
+    }
+    public String obtenerTiempo(Duration tiempo) {
+        int horas = (int) tiempo.toHours();
+        int minutos = (int) tiempo.toMinutes();
+        int segundos = (int) tiempo.toSeconds();
+
+        if (segundos > 59)
+            segundos = segundos % 60;//si los segundos son 62, en el tiempo actual muestra el residuo(02 segundos)
+        if (minutos > 59)
+            minutos = minutos % 60;//si los mintuos son 62, en el tiempo actual muestra el residuo(02 minutos)
+        if (horas > 59) horas = horas % 60;//si los horas son 62, en el tiempo actual muestra el residuo(02 horas)
+
+        if (horas > 0) return String.format("%d:%02d:%02d",
+                horas,
+                minutos,
+                segundos);
+        else return String.format("%02d:%02d:%02d",
+                horas,
+                minutos,
+                segundos);
+    }
+
 
     public void playVideo() {
         mediaPlayer.play();
