f7b5ed2930c39b7f2ff07ba99e9980e627f3b99d
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-05 21:16:19 -0600

    Se creo la funcion volverPrincipal y se añadio la funcion al boton en el FXML

--- HomeFlix/src/main/java/view/ControllerAgregarVideo.java
+++ HomeFlix/src/main/java/view/ControllerAgregarVideo.java
@@ -1,5 +1,25 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ControllerAgregarVideo {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void volverPrincipal(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
