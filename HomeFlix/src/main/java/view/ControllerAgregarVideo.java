607e6103d22d2ad9b24231e3f95b3ffd2edcb406
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-06 02:31:57 -0600

    Se añadio la funcion buscarVideo en la clase ControllerAgregarVideo

--- HomeFlix/src/main/java/view/ControllerAgregarVideo.java
+++ HomeFlix/src/main/java/view/ControllerAgregarVideo.java
@@ -1,15 +1,35 @@
 package view;
 
+import DBConexion.DAOVideoImpl;
+import Interfaces.DAOVideo;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class ControllerAgregarVideo {
+
+    @FXML
+    private TextField nombreTF;
+
+    @FXML
+    private TextField categoriaTF;
+
+    @FXML
+    private TextField descripcionTF;
+
+    @FXML
+    private Label videoLB;
 
     private Stage stage;
     private Scene scene;
@@ -21,5 +41,19 @@ public class ControllerAgregarVideo {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+    public void buscarVideo(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Buscar Video");
+
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("MP4", "*.mp4")
+        );
+
+        File vidFile = fileChooser.showOpenDialog(null);
+        if (vidFile != null){
+            videoLB.setText(vidFile.toString());
+
+        }
     }
 }
