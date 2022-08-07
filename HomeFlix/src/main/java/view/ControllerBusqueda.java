afc472d5ab48b880d474d4ee497fc81e224c1254
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-07 00:20:42 -0600

    Se añadio la funcion initialize y se agrego los atributos para una tabla

--- HomeFlix/src/main/java/view/ControllerBusqueda.java
+++ HomeFlix/src/main/java/view/ControllerBusqueda.java
@@ -1,30 +1,53 @@
 package view;
 
+import DBConexion.DAOVideoImpl;
+import Interfaces.DAOVideo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Video;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ControllerBusqueda {
+public class ControllerBusqueda implements Initializable{
     private Stage stage;
     private Scene scene;
     private Parent root;
 
     @FXML
-    private TextField nombreVideo;
+    private TextField nombreVideoTF;
     @FXML
-    private TextField categoria;
+    private TextField categoriaTF;
+    @FXML
+    private TableColumn<Video, String> nombreCO;
 
-    public void botonBuscarVideo(){
+    @FXML
+    private TableColumn<Video, String> categoriaCO;
 
-    }
+    @FXML
+    private TableColumn<Video, Double> duracionCO;
+    @FXML
+    private TableColumn<Video, Double> locaizacionCO;
+
+    @FXML
+    private TableView<Video> tblVideos;
+
+    private ObservableList<Video> videosList;
 
     public void volverPrincipal(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Principal.fxml"));
@@ -32,5 +55,25 @@ public class ControllerBusqueda {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DAOVideo daoVideo = new DAOVideoImpl();
+        videosList = FXCollections.observableArrayList();
+        ArrayList<Video> videosSeleccionados = null;
+        try {
+            videosSeleccionados = daoVideo.listarVideos();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        videosList.setAll(videosSeleccionados);
+        this.nombreCO.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.categoriaCO.setCellValueFactory(new PropertyValueFactory<>("category"));
+        this.duracionCO.setCellValueFactory(new PropertyValueFactory<>("duracion"));
+        this.locaizacionCO.setCellValueFactory(new PropertyValueFactory<>("localizacion"));
+
+        tblVideos.setItems(videosList);
+
     }
 }
