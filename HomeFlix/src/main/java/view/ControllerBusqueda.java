328dd3f85870e907dc04801b49b9e92c15eeaab1
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-07 17:22:02 -0600

    Se agrego la funcion seleccionarVideo

--- HomeFlix/src/main/java/view/ControllerBusqueda.java
+++ HomeFlix/src/main/java/view/ControllerBusqueda.java
@@ -57,6 +57,19 @@ public class ControllerBusqueda implements Initializable{
         stage.show();
     }
 
+    public void seleccionarVideo(ActionEvent event) throws IOException {
+        ObservableList<Video> videoSeleccionado;
+        videoSeleccionado = tblVideos.getSelectionModel().getSelectedItems();
+        String path = videoSeleccionado.get(0).getLocalizacion();
+        ControllerReproductor.setPath(path);
+
+        root = FXMLLoader.load(getClass().getResource("Reproductor.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public void buscarVideo(){
         DAOVideo daoVideo = new DAOVideoImpl();
         videosList = FXCollections.observableArrayList();
