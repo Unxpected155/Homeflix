08b78734f4b9bc43dcec5a5acef43cf8e94c13d8
Luis Diego <luisitobando820@gmail.com>
2022-08-06 19:59:41 -0600

    Se crearon los files para agregar a los botones en el Reproductor.fxml.

--- HomeFlix/src/main/java/view/ControllerReproductor.java
+++ HomeFlix/src/main/java/view/ControllerReproductor.java
@@ -11,8 +11,11 @@ import javafx.scene.chart.ValueAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.media.MediaView;
@@ -24,8 +27,141 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
 
-public class ControllerReproductor{
+public class ControllerReproductor implements Initializable {
 
-    
+    @FXML
+    private MediaView mvVideo;
 
+    @FXML
+    private VBox vBoxParent;
+
+    private  MediaPlayer mpVideo;
+
+    private  Media mediaVideo;
+
+    @FXML
+    private HBox hboxControls;
+
+    @FXML
+    private  HBox hboxVolume;
+
+    @FXML
+    private Button buttonPPR;
+
+    @FXML
+    private  Label labelCurrentTime;
+
+    @FXML
+    private Label labelTotalTime;
+
+    @FXML
+    private  Label labelSpeed;
+
+    @FXML
+    private Label labelVolume;
+
+    @FXML
+    private Slider sliderVolume;
+
+    @FXML
+    private Slider sliderTime;
+
+    private boolean atEndOfVideo = false;
+
+    private  boolean isPlaying = true;
+
+    private boolean isMuted = true;
+
+    private  ImageView ivPlay;
+
+    private  ImageView ivPause;
+
+    private  ImageView ivRestart;
+
+    private  ImageView ivVolume;
+
+    private  ImageView ivFullScreen;
+
+    private  ImageView ivMute;
+
+    private  ImageView ivExit;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        mediaVideo = new Media(new File("D:\\Fotos & videos\\Jre-legacy\\Chang_gritando.mp4").toURI().toString());
+        mpVideo = new MediaPlayer(mediaVideo);
+        mvVideo.setMediaPlayer(mpVideo);
+
+        final  int ivTamano = 25;
+
+        Image imagePlay = new Image(new File("").toURI().toString());
+        ivPlay = new ImageView(imagePlay);
+        ivPlay.setFitHeight(ivTamano);
+        ivPlay.setFitWidth(ivTamano);
+
+        Image imageStop = new Image(new File("").toURI().toString());
+        ivPause = new ImageView(imagePlay);
+        ivPause.setFitHeight(ivTamano);
+        ivPause.setFitWidth(ivTamano);
+
+        Image imageRestart = new Image(new File("").toURI().toString());
+        ivRestart = new ImageView(imagePlay);
+        ivRestart.setFitHeight(ivTamano);
+        ivRestart.setFitWidth(ivTamano);
+
+        Image imageVol = new Image(new File("").toURI().toString());
+        ivVolume = new ImageView(imagePlay);
+        ivVolume.setFitHeight(ivTamano);
+        ivVolume.setFitWidth(ivTamano);
+
+        Image imageFull = new Image(new File("").toURI().toString());
+        ivFullScreen = new ImageView(imagePlay);
+        ivFullScreen.setFitHeight(ivTamano);
+        ivFullScreen.setFitWidth(ivTamano);
+
+        Image imageMute = new Image(new File("").toURI().toString());
+        ivMute = new ImageView(imagePlay);
+        ivMute.setFitHeight(ivTamano);
+        ivMute.setFitWidth(ivTamano);
+
+        Image imageExit = new Image(new File("").toURI().toString());
+        ivExit = new ImageView(imagePlay);
+        ivExit.setFitHeight(ivTamano);
+        ivExit.setFitWidth(ivTamano);
+
+        buttonPPR.setGraphic(ivPause);
+        labelVolume.setGraphic(ivMute);
+        labelSpeed.setText("1X");
+
+        hboxVolume.getChildren().remove(sliderVolume);
+        mpVideo.volumeProperty().bindBidirectional(sliderVolume.valueProperty());
+
+    }
+
+//    public void bindCurrentTimeLabel(){
+//
+//        labelCurrentTime.textProperty().bind(Bindings.createStringBinding(new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                return getTime(mpVideo.getCurrentTime() + " / ");
+//            }
+//        }, mpVideo.currentTimeProperty()));
+//
+//    }
+
+//    public String getTime(Duration time){
+//
+//        int hours = (int) time.toHours();
+//        int minutes = (int) time.toMinutes();
+//        int seconds = (int) time.toSeconds();
+//
+//        if (seconds > 59) {
+//
+//            minutes = seconds % 60;
+//        }
+//
+//
+//
+//    }
 }
--- HomeFlix/src/main/java/view/Main.java
+++ HomeFlix/src/main/java/view/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Reproductor.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
