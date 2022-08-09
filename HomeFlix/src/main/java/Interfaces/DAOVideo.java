6076fc04fb051b1cf1e43cb6672f383ff2ba063e
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-08 18:01:38 -0600

    Se modificaron las funciones de la interfaz debido a que una columna en la base de datos se tiene que leer en un tipo de dato String

--- HomeFlix/src/main/java/Interfaces/DAOVideo.java
+++ HomeFlix/src/main/java/Interfaces/DAOVideo.java
@@ -6,7 +6,7 @@ import model.Video;
 import java.util.ArrayList;
 
 public interface DAOVideo {
-    public void agregarVideos(String nombre, String categoria, double duracion, String dia, String descripcion, String localizacion) throws Exception;
+    public void agregarVideos(String nombre, String categoria, String duracion, String dia, String descripcion, String localizacion) throws Exception;
     public ArrayList<Video> listarVideos() throws Exception;
     public ArrayList<Video> listarVideoNC(String nombre, String categoria) throws Exception;
 }
