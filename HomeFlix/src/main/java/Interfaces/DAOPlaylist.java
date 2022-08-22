ea5e587463759339a67f8dac2370a21337b1513d
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-22 14:47:38 -0600

    Se agrego la funcion agregarPlaylist

--- HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
+++ HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 public interface DAOPlaylist {
     public ArrayList<Playlist> listarPlaylists() throws Exception;
 
-
+    public void agregarPlaylist(String nombre, String duracion, String fechaCreacion) throws Exception;
 
 }
