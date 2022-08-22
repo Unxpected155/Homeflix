5884b31fbe54e0b02f46a806476de49808fb9482
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-22 14:47:18 -0600

    Se crea la interfaz DAOPlaylist

---
+++ HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import model.Playlist;
+import java.util.ArrayList;
+
+public interface DAOPlaylist {
+    public ArrayList<Playlist> listarPlaylists() throws Exception;
+
+
+
+}
