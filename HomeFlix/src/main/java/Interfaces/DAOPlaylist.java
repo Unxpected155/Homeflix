114dc3a8df3984c3d71c1e1c51fbea0e289c054b
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-22 15:18:36 -0600

    Se modifico la interfaz DAOPlaylist

--- HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
+++ HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public interface DAOPlaylist {
     public ArrayList<Playlist> listarPlaylists() throws Exception;
 
-    public void agregarPlaylist(String nombre, String duracion, String fechaCreacion) throws Exception;
+    public void agregarPlaylist(String nombre, String duracion, String fechaCreacion, int idUsuario) throws Exception;
 
     public void agregarVideoPlaylist(String path, String nombrePlaylist) throws Exception;
 
