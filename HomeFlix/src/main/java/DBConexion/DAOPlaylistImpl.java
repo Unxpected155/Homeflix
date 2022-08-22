353d9e63cca528f58c0fc7594cfccf4b01a0e712
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-22 15:19:19 -0600

    Se agrego cuerpo a la funcion agregarPlaylist

--- HomeFlix/src/main/java/DBConexion/DAOPlaylistImpl.java
+++ HomeFlix/src/main/java/DBConexion/DAOPlaylistImpl.java
@@ -14,7 +14,19 @@ public class DAOPlaylistImpl extends DBConexion implements DAOPlaylist {
 
     @Override
     public void agregarPlaylist(String nombre, String duracion, String fechaCreacion, int idUsuario) throws Exception {
-
+        try{
+            this.conectar();
+            PreparedStatement prs = this.conexion.prepareStatement("INSERT INTO playlist (nombre, duracion, fechaCreacion, usuario_idusuario) VALUES(?,?,?,?) ");
+            prs.setString(1, nombre);
+            prs.setString(2, duracion);
+            prs.setString(3, fechaCreacion);
+            prs.setInt(4, idUsuario);
+            prs.executeUpdate();
+        } catch(Exception e){
+            throw e;
+        } finally {
+            this.cerrar();
+        }
     }
 
     @Override
