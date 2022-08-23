a769bdabd66538c88abef3dd75a3f2b5c098ddef
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-23 01:45:49 -0600

    Se implemento la funcion initialize que permitira a la tabla de playlist estar inicializado a la hora de ingresar al FXML

--- HomeFlix/src/main/java/view/ControllerPrincipal.java
+++ HomeFlix/src/main/java/view/ControllerPrincipal.java
@@ -1,33 +1,61 @@
 package view;
 
+import DBConexion.DAOPlaylistImpl;
+import Interfaces.DAOPlaylist;
 import Threads.ProcesoServidor;
 import com.almasb.fxgl.entity.action.Action;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Playlist;
+
 import java.io.*;
 import java.net.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ControllerPrincipal {
-    @FXML
-    private TreeView ultimosVideos;
+public class ControllerPrincipal implements Initializable {
 
     @FXML
-    private TreeView listasVideos;
+    private TableView<Playlist> tblPlaylist;
+
+    @FXML
+    private TableColumn<Playlist, String> nombreCO;
+
+    @FXML
+    private TableColumn<Playlist, String> fechaCO;
+
+    @FXML
+    private Label usuario;
 
     @FXML
     private Button startHost;
 
     @FXML
     private Button joinServer;
+
+    private ObservableList<Playlist> playlistList;
+
+    public static String username;
+
+    public static String getUsername() {
+        return username;
+    }
+
+    public static void setUsername(String username) {
+        ControllerPrincipal.username = username;
+    }
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -104,5 +132,24 @@ public class ControllerPrincipal {
 
         levantarServidor.start();
 
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        usuario.setText("Bienvenido " + username);
+        DAOPlaylist daoPlaylist = new DAOPlaylistImpl();
+        playlistList = FXCollections.observableArrayList();
+        ArrayList<Playlist> playlistUsuario = null;
+        try {
+            playlistUsuario = daoPlaylist.listarPlaylists(ControllerLogin.getIdUsuarioIngresado());
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        playlistList.setAll(playlistUsuario);
+        this.nombreCO.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        this.fechaCO.setCellValueFactory(new PropertyValueFactory<>("fechaCreacion"));
+
+        tblPlaylist.setItems(playlistList);
     }
 }
