7a711931b89d996ea457ff2f72dd92e395e3eaeb
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-23 03:16:19 -0600

    Se creo la clase ControllerSeleccionarVideoPlaylist y se implemento la funcion initialize

---
+++ HomeFlix/src/main/java/view/ControllerSeleccionarVideoPlaylist.java
@@ -0,0 +1,83 @@
+package view;
+
+import DBConexion.DAOVideoImpl;
+import Interfaces.DAOVideo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.Playlist;
+import model.Video;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ControllerSeleccionarVideoPlaylist implements Initializable {
+
+    @FXML
+    private TableView<Video> tblVideos;
+
+    @FXML
+    private TableColumn<Video, String> nombreCO;
+
+    @FXML
+    private TableColumn<Video, String> categoriaCO;
+
+    @FXML
+    private TableColumn<Video, String> localizacionCO;
+
+    @FXML
+    private TableColumn<Video, String> duracionCO;
+
+    @FXML
+    private Label namePlaylist;
+
+    private ObservableList<Video> videosList;
+
+    public static String nombrePlaylist;
+
+    public static String getNombrePlaylist() {
+        return nombrePlaylist;
+    }
+
+    public static void setNombrePlaylist(String nombrePlaylist) {
+        ControllerSeleccionarVideoPlaylist.nombrePlaylist = nombrePlaylist;
+    }
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        namePlaylist.setText("Playlist " + nombrePlaylist);
+        DAOVideo daoVideo = new DAOVideoImpl();
+        videosList = FXCollections.observableArrayList();
+        ArrayList<Video> videosPlaylist;
+        try {
+            videosPlaylist = daoVideo.listarVideoPlaylist(ControllerSeleccionarVideoPlaylist.getNombrePlaylist());
+        } catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        videosList.setAll(videosPlaylist);
+        this.nombreCO.setCellValueFactory(new PropertyValueFactory<>("name"));
+        this.categoriaCO.setCellValueFactory(new PropertyValueFactory<>("category"));
+        this.duracionCO.setCellValueFactory(new PropertyValueFactory<>("duracion"));
+        this.localizacionCO.setCellValueFactory(new PropertyValueFactory<>("localizacion"));
+
+        tblVideos.setItems(videosList);
+    }
+}
