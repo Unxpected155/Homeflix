893d9f24fa8705b2e9a644df640b05de87b3b038
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-24 11:30:12 -0600

    Se realizo el javadoc de la funcion agregarPlaylist

--- HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
+++ HomeFlix/src/main/java/Interfaces/DAOPlaylist.java
@@ -5,13 +5,21 @@ import java.util.ArrayList;
 
 public interface DAOPlaylist {
     /**
-     *
+     * @author Michael NG
      * @param idUsuario recibe un parametro de tipo integer para poder identificar cual es el usuario ingresado
      * @return retorna una ArrayList de tipo playlist para poder ver cuales son los playlists del usuario ingresaod
      * @throws Exception En caso de que el query tenga un error
      */
     public ArrayList<Playlist> listarPlaylists(int idUsuario) throws Exception;
 
+    /**
+     * @author Michael Ng
+     * @param nombre recibe un parametro de tipo String y es el nombre de la playlist a agregar
+     * @param duracion recibe un parametro de tipo String y es la duracion de la playlist
+     * @param fechaCreacion recibe un parametro de tipo String y es la fecha de creacion de la playlist
+     * @param idUsuario recibe un parametro de tipo int y es para identificar cual es el usuario que esta creando la playlist
+     * @throws Exception En caso de que el query tenga un error
+     */
     public void agregarPlaylist(String nombre, String duracion, String fechaCreacion, int idUsuario) throws Exception;
 
     public void agregarVideoPlaylist(int idPlaylist, int idVideo) throws Exception;
