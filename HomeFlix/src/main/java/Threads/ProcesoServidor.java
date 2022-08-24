38a966018f0704824486eea73c9075648330bc9e
Michael Ng <kngz@ucenfotec.ac.cr>
2022-08-24 09:52:54 -0600

    se modifico la funcion run en el ProcesoServidor

--- HomeFlix/src/main/java/Threads/ProcesoServidor.java
+++ HomeFlix/src/main/java/Threads/ProcesoServidor.java
@@ -1,5 +1,7 @@
 package Threads;
 
+import view.ControllerReproductor;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -33,37 +35,103 @@ public class ProcesoServidor extends Thread{
                 Socket client = server.accept();
 
                 System.out.println("Se recibió una conexion en el puerto 8080");
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
                 PrintWriter out = new PrintWriter(client.getOutputStream(), true);
-
-                fromClient = in.readLine();
-                System.out.println("Recibido" + fromClient);
-
-                if (fromClient.equals("Hola")) {
-                    toClient = "aloH";
-                    System.out.println("enviar aloH");
-                    System.out.println(toClient);
-                    fromClient = in.readLine();
-                    System.out.println("Recibido " + fromClient);
-
-                    if (fromClient.equals("Bye")){
-
-                        toClient = "eyB";
-                        System.out.println("enviar eyB");
-                        out.println(toClient);
-                        client.close();
-                        run = false;
-                        System.out.println("Socket cerrado");
-                    }
-                }
+                out.println(ControllerReproductor.getPath());
+                client.close();
+                server.close();
+                run = false;
             }
 
         }catch (IOException e){
 
             throw new RuntimeException(e);
         }
-
-
-        System.exit(0);
     }
+
+    public static void play(){
+        String fromClient;
+        String toClient;
+
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(8080);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Esperando conexion en el puerto 8080");
+
+        boolean run = true;
+
+        try {
+            while (run){
+                Socket client = server.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                out.println("play");
+                client.close();
+                server.close();
+                run = false;
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void pause(){
+        String fromClient;
+        String toClient;
+
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(8080);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Esperando conexion en el puerto 8080");
+
+        boolean run = true;
+
+        try {
+            while (run){
+                Socket client = server.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                out.println("pause");
+                client.close();
+                run = false;
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void restart(){
+        String fromClient;
+        String toClient;
+
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(8080);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Esperando conexion en el puerto 8080");
+
+        boolean run = true;
+
+        try {
+            while (run){
+                Socket client = server.accept();
+                PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                out.println("restart");
+                client.close();
+                server.close();
+                run = false;
+            }
+        }catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
